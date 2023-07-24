2ca563af9ee71b36ebc9a6c9b5017c1235654fc9
Younus paswar <yunuspanwar78@gmail.com>
2023-07-24 23:38:53 +0530

    learn js work

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// var a = "hello world";
+// var a ="yunus";
+// a = "mustafa";
+
+// console.log(a);
+
+// let a = 10;
+// a = 20;
+// console.log(a); 
+
+// const a ="yumud";
+// console.log(a);
