4180ccca06f34f21e648f8775502c9675b6b5447
Younus paswar <yunuspanwar78@gmail.com>
2023-08-06 13:42:20 +0530

    learn new method in old files

--- js/statement.js
+++ js/statement.js
@@ -1,6 +1,55 @@
-let a = 200;
-let b = 300;
+// Greater
+let a = 30;
+let b = 20;
 
-if(a > b){
-    console.log("a is big");
+if (a > b) {
+    console.log("A is a big");
+}
+
+// Lessthen
+let c = 50;
+let d = 100;
+
+if(c < d) {
+    console.log("d is a big");
+}
+
+// Equal
+let e = 20;
+let f = 20;
+
+if (e == f){
+    console.log("Equal");
+}
+
+// Data Type With Equal
+let h = 10;
+let g = "10";
+
+if(h===g) {
+    console.log("data type equal");
+}
+
+// Statement Logical Operator
+
+// Logical And
+let age = 10;
+
+if (age >= 8 && age <= 11) {
+    console.log("Yes You Are Eligible" );
+}
+
+// Logical OR
+let i = 10;
+let j = 15;
+
+if (i >= 10 || j <= 15 ) {
+    console.log("Not");
+}
+
+// Logical Not
+let k = 10;
+
+if (k >= 5) {
+    console.log(Logical);
 }
---
+++ js/ternary.js
@@ -0,0 +1,6 @@
+let a = 15;
+let b;
+
+(a>10)? b= "ture": b= "false";
+
+console.log(b);
