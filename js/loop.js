a509dae36d239d60bea0135bf8b4e6c30bb0d2ea
Younus paswar <yunuspanwar78@gmail.com>
2023-08-18 19:13:29 +0530

    learn new method

--- js/loop.js
+++ js/loop.js
@@ -1,5 +1,32 @@
-let a = 1;
+// While Loop
 
-while (a <= 1) {
-    console.log("yahoo baba");
+// let a = 1;
+
+// while (a <= 10) {
+//     console.log(a);
+//     a++
+// }
+
+// Do While Loop
+
+// let b = 1;
+
+// do {
+//     console.log("yaasu");
+//     b = b + 2; 
+// } while (b <= 10);
+
+
+let a  = 10;
+
+while (a <= 100) {
+    console.log("hey guys");
+    a=a+2;
 }
+
+let b = 20;
+
+do {
+    console.log("hlo");
+    a++
+} while (b <= 25);
